f2a20c8937ece58212c1f06f457c170388ff6060
Usama Arain <ua339716@gmail.com>
2023-10-05 23:12:20 -0700

    js2

---
+++ app.js
@@ -0,0 +1,35 @@
+var userName = "Usama123"
+alert(userName)
+
+var firstName = "Usama"
+var lastName = " Arain"
+var fullName = firstName + lastName
+alert(fullName)
+
+var titled = "Hello World"
+alert(titled)
+
+var myName = "Usama Arain"
+alert(myName)
+
+var myAge = "25 Years old"
+alert(myAge)
+
+var certificate = "Certificate Mobile Application Development"
+alert(certificate)
+
+var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP"
+alert(pizza)
+
+var email = "My Email Address is ua339716@gmail.com"
+alert(email)
+
+var book = "I am trying to learn from the Book A smarter way to learn Javascript"
+alert(book)
+
+var text = "Yah! I can write HTML content  through Javascript"
+document.write(text)
+
+var store = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(store)
+document.write(store)
